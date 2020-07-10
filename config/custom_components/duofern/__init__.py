bc5fa6c0a773cd7d3647dabd2a9f0cdbbdceb082
Markus Haack <markus.haack@gmail.com>
2020-07-10 16:06:58 +0200

    Plugin & component updates

--- config/custom_components/duofern/__init__.py
+++ config/custom_components/duofern/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 # from homeassistant.const import 'serial_port', 'config_file', 'code'
 import homeassistant.helpers.config_validation as cv
@@ -10,7 +11,7 @@ from homeassistant.helpers import discovery
 # Import the device class from the component that you want to support
 
 # Home Assistant depends on 3rd party packages for API specific code.
-REQUIREMENTS = ['pyduofern']
+REQUIREMENTS = ['pyduofern==0.30.0']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,9 +19,11 @@ from .const import DOMAIN, DUOFERN_COMPONENTS, CONF_SERIAL_PORT, CONF_CODE
 
 # Validation of the user's configuration
 CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({
-    vol.Optional('serial_port', default="/dev/serial/by-id/usb-Rademacher_DuoFern_USB-Stick_WR04ZFP4-if00-port0"): cv.string,
-    vol.Optional('config_file', default=None): cv.string,
-    vol.Optional('code', default="deda"): cv.string,
+    vol.Optional('serial_port',
+                 default="/dev/serial/by-id/usb-Rademacher_DuoFern_USB-Stick_WR04ZFP4-if00-port0"): cv.string,
+    vol.Optional('config_file', default=os.path.join(os.path.dirname(__file__), "../../duofern.json")): cv.string,
+    # config file: default to homeassistant config directory (assuming this is a custom component)
+    vol.Optional('code', default="0000"): cv.string,
 }),
 }, extra=vol.ALLOW_EXTRA)
 
@@ -37,18 +40,26 @@ def setup(hass, config):
 
     from pyduofern.duofern_stick import DuofernStickThreaded
 
-    if config.get(DOMAIN) is not None:
+    newstyle_config = hass.config_entries.async_entries(DOMAIN)
+    if len(newstyle_config) > 0:
+        newstyle_config = newstyle_config[0]
+        if newstyle_config:
+            serial_port = newstyle_config.data['serial_port']
+            code = newstyle_config.data['code']
+            configfile = newstyle_config.data['config_file']
+
+    elif config.get(DOMAIN) is not None:
         serial_port = config[DOMAIN].get(CONF_SERIAL_PORT)
         if serial_port is None:
             serial_port = "/dev/serial/by-id/usb-Rademacher_DuoFern_USB-Stick_WR04ZFP4-if00-port0"
-        code = config[DOMAIN].get(CONF_CODE)
+        code = config[DOMAIN].get(CONF_CODE, None)
         if code is None:
             code = "affe"
         configfile = config[DOMAIN].get('config_file')
 
     hass.data[DOMAIN] = {
         'stick': DuofernStickThreaded(serial_port=serial_port, system_code=code, config_file_json=configfile,
-                                      ephemeral=True),
+                                      ephemeral=False),
         'devices': {}}
     hass.data[DOMAIN]['stick'].start()
 
@@ -74,11 +85,10 @@ def setup(hass, config):
 
     def clean_config(call):
         stick.clean_config()
-        stick.sync_cevides()
+        stick.sync_devices()
 
     hass.services.register(DOMAIN, 'sync_devices', sync_devices)
-    hass.services.register(DOMAIN, 'clean_config', sync_devices)
-
+    hass.services.register(DOMAIN, 'clean_config', clean_config)
 
     def refresh(call):
         _LOGGER.warning(call)
--- config/custom_components/duofern/light.py
+++ config/custom_components/duofern/light.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 # from homeassistant.const import 'serial_port', 'config_file', 'code'
 import homeassistant.helpers.config_validation as cv
@@ -6,7 +7,7 @@ import voluptuous as vol
 # found advice in the homeassistant creating components manual
 # https://home-assistant.io/developers/creating_components/
 # Import the device class from the component that you want to support
-from homeassistant.components.light import Light, PLATFORM_SCHEMA
+from homeassistant.components.light import LightEntity, PLATFORM_SCHEMA, SUPPORT_BRIGHTNESS, ATTR_BRIGHTNESS
 
 # Home Assistant depends on 3rd party packages for API specific code.
 
@@ -31,20 +32,36 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     stick = hass.data["duofern"]['stick']
 
     # Add devices
-    to_add = [DuofernLight(device['id'], device['name'], stick, hass) for device in stick.config['devices'] if
-              not device['id'].startswith('46') and not device['id'] in hass.data[DOMAIN]['devices'].keys()]
-    add_devices(to_add)
+    for device in stick.config['devices']:
+        if device['id'].startswith('46') or device['id'].startswith('48'):
+            if device['id'] in hass.data[DOMAIN]['devices'].keys():
+                continue
+            add_devices([DuofernLight(device['id'], device['name'], stick, hass)])
+
+        if device['id'].startswith('43'):
+            for channel in [1,2]:
+                chanNo = "{:02x}".format(channel)
+                if device['id']+chanNo in hass.data[DOMAIN]['devices'].keys():
+                    continue
+                add_devices([DuofernLight(device['id'], device['name'], stick, hass, channel=channel)])
 
 
-class DuofernLight(Light):
-    def __init__(self, id, desc, stick, hass):
-        """Initialize the shutter."""
-        self._id = id
+class DuofernLight(LightEntity):
+    def __init__(self, code, desc, stick, hass, channel=None):
+        """Initialize the light."""
+        self._code = code
+        self._id = code
         self._name = desc
+
+        if channel:
+          chanNo = "{:02x}".format(channel)
+          self._id += chanNo
+          self._name += chanNo
+
         self._state = None
-        self._brightness = None
         self._stick = stick
-        hass.data[DOMAIN]['devices'][id] = self
+        self._channel = channel
+        hass.data[DOMAIN]['devices'][self._id] = self
 
     @property
     def name(self):
@@ -53,20 +70,54 @@ class DuofernLight(Light):
     @property
     def is_on(self):
         try:
-            _LOGGER.info(self._stick.duofern_parser.modules['by_code'][self._id])
-            return self._stick.duofern_parser.modules['by_code'][self._id]['state'] == "on"
+            state = self._stick.duofern_parser.get_state(self._code, 'state', channel=self._channel)
+            return state != "off"
         except KeyError:
             return None
 
-    def turn_on(self):
-        self._stick.command(self._id, "on")
-        # this is a hotfix because currently the state is not correctly detected from duofern
-        self._stick.duofern_parser.modules['by_code'][self._id]['state'] = "on"
+    @property
+    def unique_id(self):
+        return self._id
+
+    @property                
+    def brightness(self):
+        if self._code.startswith("48"):
+            level = self._stick.duofern_parser.get_state(self._code, 'level', channel=self._channel)
+            if level == None:
+                return None
+
+            return math.ceil(int(level)/100.0*255)
+        else:
+            return None
+
+    @property
+    def supported_features(self):
+        if self._code.startswith("48"):
+            return SUPPORT_BRIGHTNESS
+        else:
+            return 0
+
+    def turn_on(self,**kwargs):
+        if self._code.startswith("48"):
+            brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
+            level = math.ceil(brightness/255.0*100)
+            if ATTR_BRIGHTNESS in kwargs:
+                self._stick.command(self._code, "level", level, channel=self._channel)
+            else:
+                self._stick.command(self._code, "on", channel=self._channel) # dim slowly up
+
+            # this is a hotfix because currently the state is detected with delay from duofern
+            self._stick.duofern_parser.update_state(self._code, 'level', str(level), channel=self._channel)
+            self._stick.duofern_parser.update_state(self._code, 'state', str(level), channel=self._channel)
+        else:
+            self._stick.command(self._code, "on", channel=self._channel)
+            # this is a hotfix because currently the state is detected with delay from duofern
+            self._stick.duofern_parser.update_state(self._code, 'state', "on", channel=self._channel)
 
     def turn_off(self):
-        self._stick.command(self._id, "off")
-        # this is a hotfix because currently the state is not correctly detected from duofern
-        self._stick.duofern_parser.modules['by_code'][self._id]['state'] = 0
+        self._stick.command(self._code, "off", channel=self._channel)
+        # this is a hotfix because currently the state is detected with delay from duofern
+        self._stick.duofern_parser.update_state(self._code, 'state', "off", channel=self._channel)
 
     def update(self):
         pass
