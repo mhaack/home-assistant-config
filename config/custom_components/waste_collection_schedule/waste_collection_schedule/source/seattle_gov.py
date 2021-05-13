c5c3e023c9814b504f46610c3906ef6ff90ee370
Markus Haack <markus.haack@gmail.com>
2021-05-13 14:06:49 +0200

    Update ♻️ waste collection

--- config/custom_components/waste_collection_schedule/waste_collection_schedule/source/seattle_gov.py
+++ config/custom_components/waste_collection_schedule/waste_collection_schedule/source/seattle_gov.py
@@ -1,7 +1,5 @@
 import datetime
 import json
-import time
-from urllib.parse import quote
 
 import requests
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
@@ -11,9 +9,14 @@ DESCRIPTION = "Source for Seattle Public Utilities waste collection."
 URL = "https://myutilities.seattle.gov/eportal/#/accountlookup/calendar"
 TEST_CASES = {
     "City Hall": {"street_address": "600 4th Ave"},
-    "Honey Hole": {"street_address": "703 E Pike St"},
+    "Ballard Builders": {"street_address": "7022 12th Ave NW"},
     "Carmona Court": {"street_address": "1127 17th Ave E"},
 }
+
+
+def get_service_icon(service_name):
+    switcher = {"Garbage": "trash-can", "Recycle": "recycle", "Food/Yard Waste": "leaf"}
+    return switcher.get(service_name, "trash-can")
 
 
 class Source:
@@ -21,40 +24,114 @@ class Source:
         self._street_address = street_address
 
     def fetch(self):
-        start_time = int(time.time())
 
-        # get json file
-        r = requests.get(
-            f"https://www.seattle.gov/UTIL/WARP/CollectionCalendar/GetCollectionDays?pApp=CC&pAddress={quote(self._street_address)}&start={start_time}"
+        # Mimicking the same API calls the calendar lookup page uses:
+        # 1. find account code
+        # 2. find account ID for a given address
+        # 3. get token (uses basic auth and the customerID = guest
+        # 4. get account summary
+        # 5. get calendar
+
+        # step 1
+        find_address_payload = {
+            "address": {"addressLine1": self._street_address, "city": "", "zip": ""}
+        }
+
+        r = requests.post(
+            "https://myutilities.seattle.gov/rest/serviceorder/findaddress",
+            json=find_address_payload,
         )
 
-        # extract data from json
-        data = json.loads(r.text)
-        next_pickup = data[0]
+        address_info = json.loads(r.text)
+        prem_code = address_info["address"][0]["premCode"]
 
-        if not next_pickup["start"]:
-            return []
+        # step 2
+        find_account_payload = {"address": {"premCode": prem_code}}
 
-        next_pickup_date = datetime.datetime.strptime(
-            next_pickup["start"], "%a, %d %b %Y"
-        ).date()
+        r = requests.post(
+            "https://myutilities.seattle.gov/rest/serviceorder/findAccount",
+            json=find_account_payload,
+        )
 
-        # create entries for trash, recycling, and yard waste
+        account_info = json.loads(r.text)
+        account_number = account_info["account"]["accountNumber"]
+
+        # step 3
+        token_payload = {
+            "grant_type": "password",
+            "username": "guest",
+            "password": "guest",
+        }
+
+        r = requests.post(
+            "https://myutilities.seattle.gov/rest/auth/token", data=token_payload
+        )
+
+        token_info = json.loads(r.text)
+        token = token_info["access_token"]
+
+        headers = {"Authorization": f"Bearer {token}"}
+
+        # step 4
+        swsummary_payload = {
+            "customerId": "guest",
+            "accountContext": {
+                "accountNumber": account_number,
+                "personId": None,
+                "companyCd": None,
+                "serviceAddress": None,
+            },
+        }
+
+        r = requests.post(
+            "https://myutilities.seattle.gov/rest/account/swsummary",
+            json=swsummary_payload,
+            headers=headers,
+        )
+
+        summary_info = json.loads(r.text)
+        # the description property in each service in swServices it's either 'Garbage', 'Recycle', or 'Food/Yard Waste'
+
+        swServices = summary_info["accountSummaryType"]["swServices"][0]["services"]
+        personId = summary_info["accountContext"]["personId"]
+        companyCd = summary_info["accountContext"]["companyCd"]
+
+        # step 5
+        waste_calendar_payload = {
+            "customerId": "guest",
+            "accountContext": {
+                "accountNumber": account_number,
+                "personId": personId,
+                "companyCd": companyCd,
+            },
+            "servicePoints": [],
+        }
+
+        # fill out payload
+        for service in swServices:
+            waste_calendar_payload["servicePoints"].append(service["servicePointId"])
+
+        r = requests.post(
+            "https://myutilities.seattle.gov/rest/solidwastecalendar",
+            json=waste_calendar_payload,
+            headers=headers,
+        )
+
+        calendar_info = json.loads(r.text)
+
+        # output
         entries = []
 
-        if next_pickup["Garbage"]:
-            entries.append(
-                Collection(date=next_pickup_date, t="Trash", icon="mdi:trash-can")
-            )
-        if next_pickup["FoodAndYardWaste"]:
-            entries.append(
-                Collection(
-                    date=next_pickup_date, t="Food and Yard Waste", icon="mdi:leaf"
-                )
-            )
-        if next_pickup["Recycling"]:
-            entries.append(
-                Collection(date=next_pickup_date, t="Recycling", icon="mdi:recycle")
-            )
+        for service in swServices:
+            name = service["description"]
+            servicePointId = service["servicePointId"]
+
+            for collection_date in calendar_info["calendar"][servicePointId]:
+                next_date = datetime.datetime.strptime(
+                    collection_date, "%m/%d/%Y"
+                ).date()
+                service_icon = "mdi:" + get_service_icon(name)
+
+                entries.append(Collection(date=next_date, t=name, icon=service_icon))
 
         return entries
